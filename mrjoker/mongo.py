2356ed9bb910ca7a490c3d9332976400b7925fbb
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 14:35:13 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -1,6 +1,7 @@
 import asyncio
 import sys
 
+
 from motor import motor_asyncio
 #from mrjoker import MONGO_DB_URI 
 from pymongo import MongoClient
@@ -9,7 +10,7 @@ from mrjoker.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("mongodb+srv://joker:kaj@cluster0.tmq5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "mrjoker"
 
 
@@ -17,7 +18,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["MrJOkerbot"]
+db = client["MrJokerbot"]
 
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
