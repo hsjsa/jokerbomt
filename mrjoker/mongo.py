0f655e091fce6613ebf93538ea378f903851dcd2
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 17:29:55 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -10,7 +10,7 @@ from mrjoker.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("MONGO_PORT")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "jokerbot"
+MONGO_DB = "mrjoker"
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
