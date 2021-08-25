a8a5b2c31e9da6f66cbe95d8c07c8c5c25f7002f
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 14:21:12 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -8,7 +8,7 @@ from pymongo.errors import ServerSelectionTimeoutError
 from mrjoker.conf import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27019")
+MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("mongodb+srv://joker:kaj@cluster0.tmq5o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 MONGO_DB = "mrjoker"
 
