75be780fa9531da1ee3e044a775a04fa66918c0b
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 13:38:36 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -8,9 +8,9 @@ from pymongo.errors import ServerSelectionTimeoutError
 from mrjoker.conf import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("7983")
+MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "joker"
+MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
