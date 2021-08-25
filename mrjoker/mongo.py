8fa02429c045f601b9e7de0408462b172b2a1df2
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 12:25:04 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -19,8 +19,6 @@ motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["mrjoker"]
 
-engine = AIOEngine(motor, MONGO_DB)
-
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
