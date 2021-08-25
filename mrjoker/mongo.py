dbdb3c4120b05449877e6a569a815855e1be5a40
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-25 16:12:04 +0530

    Update mongo.py

--- mrjoker/mongo.py
+++ mrjoker/mongo.py
@@ -4,20 +4,19 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from mrjoker import LOGGER
+#from mrjoker import LOGGER
 from mrjoker import MONGO_DB_URI
 from mrjoker.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
-
+MONGO_DB = "jokerbot"
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["mrjoker"]
+db = client["MrJokerbot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
