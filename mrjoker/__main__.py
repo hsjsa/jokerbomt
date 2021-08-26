3ee03dbd9913a7e4b59b4d40a1e8726d657608c5
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-26 16:30:44 +0530

    Update __main__.py

--- mrjoker/__main__.py
+++ mrjoker/__main__.py
@@ -299,7 +299,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*⚊❮❮❮❮ ｢  Help  for  {}  module 」❯❯❯❯⚊*\n".format(
+                "*⊱⚊❮❮❮❮────《∘◦ Mr.Joker Help for {} module ◦∘》────❯❯❯❯⚊⊰*\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
