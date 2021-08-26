aeb55cad7723358ec051c590c7c38d0e8473bebf
Dark Hunter <86953304+kavinduaj12@users.noreply.github.com>
2021-08-26 12:36:16 +0530

    Update and rename aimultilanguage.pytest to aimultilanguage.py

--- mrjoker/modules/aimultilanguage.py
+++ mrjoker/modules/aimultilanguage.py
@@ -1,19 +1,3 @@
-# Copyright (C) 2021 Red-Aura & TeamDaisyX & HamkerCat
-
-# This file is part of AsunaRobot (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re
 
 import emoji
@@ -28,8 +12,8 @@ from googletrans import Translator as google_translator
 from pyrogram import filters
 
 from mrjoker import BOT_ID, pbot as mrjoker
-from mrjoker.utils.aichat import add_chat, get_session, remove_chat
-from mrjoker.pyrogram.pluginshelper import admins_only, edit_or_reply
+from mrjoker.helper_extra.aichat import add_chat, get_session, remove_chat
+from mrjoker.pyrogram.pluginhelpers import admins_only, edit_or_reply
 
 translator = google_translator()
 
